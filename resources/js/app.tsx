9aeaf39edf6c1b71eb11f5263f430e1a8c525f94
sofiaPalavecino <sofiaor29@gmail.com>
2026-01-08 15:20:13 -0300

    added ziggy

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,4 +1,4 @@
-//import '../css/app.css';
+import '../css/app.css';
 import '../scss/app.scss';
 
 import { createInertiaApp } from '@inertiajs/react';
@@ -6,6 +6,10 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
+import { Ziggy } from './ziggy';
+
+(window as any).Ziggy = Ziggy;
+
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
